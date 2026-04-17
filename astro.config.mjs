e0572db7bd7496733d6757504b2f3510a9c4a562
Nash <nashtechie615@gmail.com>
2026-04-17 05:37:47 -0500

    Initial RepThe1 blog: Astro + GitHub Pages deploy workflow

---
+++ astro.config.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://nashtechie615-cyber.github.io',
+	base: '/repthe1-blog',
+	trailingSlash: 'ignore',
+	image: {
+		domains: ['repthe1.com'],
+	},
+	integrations: [mdx(), sitemap()],
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: 'Atkinson',
+			cssVariable: '--font-atkinson',
+			fallbacks: ['sans-serif'],
+			options: {
+				variants: [
+					{
+						src: ['./src/assets/fonts/atkinson-regular.woff'],
+						weight: 400,
+						style: 'normal',
+						display: 'swap',
+					},
+					{
+						src: ['./src/assets/fonts/atkinson-bold.woff'],
+						weight: 700,
+						style: 'normal',
+						display: 'swap',
+					},
+				],
+			},
+		},
+	],
+});
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'RepThe1 Blog';
+export const SITE_DESCRIPTION = 'Christian streetwear, faith devotionals, and product spotlights from RepThe1.';
+export const SHOP_URL = 'https://repthe1.com';
